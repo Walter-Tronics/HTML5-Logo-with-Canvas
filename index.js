1dafa99d9968ae5278cf7906b6769144142deeda
Walter Tronics <waltertronics02@gmail.com>
2022-08-21 08:57:38 +0100

    Draw the complete logo

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 window.onload=()=>{
 
-    //Getting the canvas element
-    ctx = document.getElementById('canvas').getContext('2d');
+    let ctx,
+    //selecting the slider element
+    slider = document.getElementById('slide'),
+    //setting a factorValue
+    factorValue = 1,
+    //Declaring a font family
+    fontFamily = "Bold 65px 'Gill Sans Ultra Bold', sans-serif";
 
 
-    //initializing the start coordinates of the shield
-    drawLogo();
+    //Inintializing the canvas
+    init();
+
+    function init(){
+        ctx = document.getElementById("canvas").getContext("2d");
+        //Setting the font family
+        ctx.font = fontFamily;
+        //Saving the context
+        ctx.save();
+        //Calling the draw function
+        drawLogo();
+    }
+
+
+    ///function for scaling the shield
+    function scaleLogo(num){
+        //setting the factor value
+        factorValue = num / 100;
+        //calling the draw function
+        drawLogo();
+    }
 
 
     //function for initializing the shield
     function drawLogo(){
         //giving an offset to the shield
         var offsetY = 80;
+        //Recall the saved context
         ctx.restore();
+        //saving the context
         ctx.save();
+        //clearing the canvas
         ctx.clearRect(0, 0, 600, 400);
+        //scaling the canvas
         ctx.scale(factorValue, factorValue);
         //Drawing the text
         ctx.fillText("HTML", 31,60);
+        ctx
         ctx.translate(0, offsetY);
 
         //Setting a color value of the shield
@@ -45,8 +74,61 @@ window.onload=()=>{
         ctx.lineTo(139,20);
         ctx.closePath();
         ctx.fill();
+
+        //first half of the five
+        ctx.fillStyle = '#EBEBEB';
+        ctx.beginPath();
+        ctx.moveTo(139,113);
+        ctx.lineTo(98,113);
+        ctx.lineTo(96,82);
+        ctx.lineTo(139,82);
+        ctx.lineTo(139,51);
+        ctx.lineTo(62,51);
+        ctx.lineTo(70,144);
+        ctx.lineTo(139,144);
+        ctx.closePath();
+        ctx.fill();
+
+        //other cut of first half
+        ctx.beginPath();
+        ctx.moveTo(139,193);
+        ctx.lineTo(105,184);
+        ctx.lineTo(103,159);
+        ctx.lineTo(72,159);
+        ctx.lineTo(76,207);
+        ctx.lineTo(139,225);
+        ctx.closePath();
+        ctx.fill();
+
+        //second half of the five
+        ctx.fillStyle = '#FFFFFF';
+        ctx.beginPath();
+        ctx.moveTo(139,113);
+        ctx.lineTo(139,144);
+        ctx.lineTo(177,144);
+        ctx.lineTo(173,184);
+        ctx.lineTo(139,193);
+        ctx.lineTo(139,225);
+        ctx.lineTo(202,207);
+        ctx.lineTo(210,113);
+        ctx.closePath();
+        ctx.fill();
+
+        //other cut of second half
+        ctx.beginPath();
+        ctx.moveTo(139,51);
+        ctx.lineTo(139,82);
+        ctx.lineTo(213,82);
+        ctx.lineTo(216,51);
+        ctx.closePath();
+        ctx.fill();
     }
 
-    //Drawing the text font
-    var fontFamily = "65px 'Gill Sans Ultra Bold', sans-serif";
+
+    //adding the event listener for the slider
+    slider.addEventListener('input', function(){
+        //calling the scale function
+        scaleLogo(slider.value);
+        console.log(slider.value);
+    });
 }
