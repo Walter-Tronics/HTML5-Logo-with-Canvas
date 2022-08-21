a456e035797e04fff4d964a7d6ad5996ea362b80
Walter Tronics <waltertronics02@gmail.com>
2022-08-21 07:40:30 +0100

    Declaring the logo co-ordinates

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 window.onload=()=>{
 
+    //Getting the canvas element
+    ctx = document.getElementById('canvas').getContext('2d');
+
+
     //initializing the start coordinates of the shield
-    initCoords();
+    drawLogo();
+
 
     //function for initializing the shield
-    function initCoords(){
-        //Getting the canvas element
-        ctx = document.getElementById('canvas').getContext('2d');
+    function drawLogo(){
+        //giving an offset to the shield
+        var offsetY = 80;
+        ctx.restore();
+        ctx.save();
+        ctx.clearRect(0, 0, 600, 400);
+        ctx.scale(factorValue, factorValue);
+        //Drawing the text
+        ctx.fillText("HTML", 31,60);
+        ctx.translate(0, offsetY);
+
         //Setting a color value of the shield
         ctx.fillStyle = '#E34C26';
         //Start the shape drawing
@@ -20,5 +33,20 @@ window.onload=()=>{
         ctx.closePath();
         //Filling the shape with the color
         ctx.fill();
+        //calling a font family
+        ctx.font = fontFamily;
+
+        //The lighter half of the shield
+        ctx.fillStyle = '#F06529';
+        ctx.beginPath();
+        ctx.moveTo(139,257);
+        ctx.lineTo(220,234);
+        ctx.lineTo(239,20);
+        ctx.lineTo(139,20);
+        ctx.closePath();
+        ctx.fill();
     }
+
+    //Drawing the text font
+    var fontFamily = "65px 'Gill Sans Ultra Bold', sans-serif";
 }
